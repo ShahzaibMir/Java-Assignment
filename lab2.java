1aa4c42cb3d82be747315adbdb89108d44ebbf83
Shahzaib Mir <94667491+ShahzaibMir@users.noreply.github.com>
2021-11-19 10:40:19 +0530

    Create lab2.java

---
+++ lab2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class lab2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = 1;
+        while(b<=a){
+            int j = 1;
+            char d = (char)('A'+b-1);
+            while(c<=b){
+                System.out.print(alpha);
+                d = (char)(d+1);
+                c = c+1;
+            }
+            System.out.println();
+            b = b+1;
+        }
+    }
+}
